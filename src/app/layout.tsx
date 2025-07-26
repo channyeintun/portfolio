c8994590ff583fdbc12ee30985b11945a87c149d
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2025-07-26 22:52:24 +0700

    fix: update styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="en" className="dark scroll-smooth">
       <body
         className={cn(
-          'min-h-screen bg-background font-sans antialiased bg-grid-pattern',
+          'min-h-screen bg-background font-sans bg-grid-pattern',
           inter.variable,
           dmSerifText.variable
         )}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,23 +6,29 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       <section className="relative flex min-h-[calc(100vh-5rem)] w-full items-center justify-center overflow-hidden">
-        <div className="absolute top-0 left-0 h-full w-full"></div>
-        <div className="container z-10 mx-auto flex max-w-5xl flex-col items-center justify-center gap-6 px-4 text-center">
+        <div className="container z-10 mx-auto flex max-w-2xl flex-col items-start justify-center gap-6">
           <div className="space-y-4">
-            <p>Hi! I'm a</p>
-            <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl font-headline">
+            <p className="max-sm:mt-8 text-sm font-extralight" style={{
+              color: "oklch(0.928 0.006 264.531)"
+            }}>Hi! I'm a</p>
+            <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl font-headline whitespace-nowrap">
               <span className="text-primary">Frontend</span> Developer
             </h1>
           </div>
-          <div className="max-w-2xl">
-            <p className="mb-6 text-lg md:text-xl">
+          <div className="">
+            <p className="mb-6 text-lg md:text-xl text-balance font-extralight" style={{
+              fontFeatureSettings: "normal",
+              fontVariationSettings: "normal",
+              color: 'white',
+              WebkitTextSizeAdjust: '100%',
+            }}>
               I&apos;m glad you&apos;re here. Take a look around and discover what I have to offer.
             </p>
-            <p className="text-muted-foreground md:text-lg">
+            <p className="text-muted-foreground text-base font-light" style={{ color: 'color(srgb 0.628235 0.628235 0.628235)' }}>
               I'm an experienced software engineer with 5 years specializing in React.js and Next.js. Passionate about building modern, responsive, and user-friendly web applications.
             </p>
           </div>
-          <div className="flex flex-col gap-4 min-[400px]:flex-row justify-center flex-wrap">
+          <div className="flex flex-col gap-4 min-[400px]:flex-row justify-start flex-wrap">
             <Button asChild size="lg" className="rounded-md bg-gradient-to-b from-white/10 bg-red-500 text-gray-100 relative shadow-inner shadow-gray-900/20 motion-safe:transition active:scale-98 grid place-items-center after:content-[''] after:absolute after:inset-0 after:rounded-md after:bg-gradient-to-b after:from-white/40 after:to-white/10 after:opacity-70 hover:after:opacity-100 after:motion-safe:transition">
               <Link href="/projects"><span className='text-shadow-lg/20 [text-shadow:0_1px_1px_#991b1b]'>View My Work</span></Link>
             </Button>
@@ -36,7 +42,7 @@ export default function Home() {
               <Link href="/contact">Get In Touch</Link>
             </Button>
           </div>
-          <div className="mt-6 flex items-center gap-6">
+          <div className="mt-6 flex items-center gap-6 max-sm:mb-6">
             <Link
               href="https://github.com/channyeintun"
               aria-label="GitHub profile"
